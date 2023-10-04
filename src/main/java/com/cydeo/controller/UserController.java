e8335a01ba8e64ea2ba471cee31ddd2eb509ac35
Mike <miksid@protonmail.com>
2023-10-03 22:17:12 -0400

    user create - services

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -13,6 +13,7 @@ public class UserController {
     @GetMapping("/create")
     public String createUser(Model model){
         model.addAttribute("user", new UserDTO());
+       // model.addAttribute("roles",listOfRoles);
 
         return "/user/create";
 
